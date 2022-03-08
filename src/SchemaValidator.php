5ddccdb48c5fdcab22cb6c76ac64bdf23e989527
Carlos C Soto <eclipxe13@gmail.com>
2022-03-08 12:43:12 -0600

    DOMAttr::nodeValue can be null, skip if this is the case

--- src/SchemaValidator.php
+++ src/SchemaValidator.php
@@ -153,9 +153,11 @@ class SchemaValidator
         $schemasList = $xpath->query("//@$xsi:schemaLocation") ?: new DOMNodeList();
 
         // process every schemaLocation and import them into schemas
-        foreach ($schemasList as $node) {
-            /** @psalm-suppress PossiblyNullArgument */
-            $schemas->import($this->buildSchemasFromSchemaLocationValue($node->nodeValue));
+        foreach ($schemasList as $schemaAttribute) {
+            $schemaValue = $schemaAttribute->nodeValue;
+            if (null !== $schemaValue) {
+                $schemas->import($this->buildSchemasFromSchemaLocationValue($schemaValue));
+            }
         }
 
         return $schemas;
